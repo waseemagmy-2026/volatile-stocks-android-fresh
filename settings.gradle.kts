e81225f7cc6b8fa1270ce01107aa9b604a631a3a
waseemagmy-2026 <waseem.agmy@gmail.com>
2026-05-09 03:37:56 +0300

    settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven("https://jitpack.io")
     }
 }
 
@@ -11,8 +12,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
-rootProject.name = "VolatileStocksAndroidPro"
+rootProject.name = "VolatileStocksAndroidReady"
 include(":app")
